e4ecc8419fac14baf9444b40d84a246718b8b210
Dmitriy <dmitriyegorov111@mail.ru>
2023-04-18 23:54:54 +0300

    Connected postgress and django, created all the necessary fields in the database

--- gameBack/gameAPI/admin.py
+++ gameBack/gameAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Theory)
+admin.site.register(Reward)
+admin.site.register(Level)
+admin.site.register(Character)
+admin.site.register(User)
--- gameBack/gameAPI/models.py
+++ gameBack/gameAPI/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Theory(models.Model):
+    topic = models.CharField(max_length=32, verbose_name="Тема вопроса")
+    content = models.CharField(max_length=256, verbose_name="Содержание вопроса")
+
+
+class Reward(models.Model):
+    caption = models.CharField(max_length=32, verbose_name="Название NFT")
+    hash = models.CharField(max_length=46, verbose_name="Хэш NFT")
+    theory = models.ForeignKey("Theory", on_delete=models.DO_NOTHING, related_name="TheoryOf")
+
+
+class Level(models.Model):
+    topic = models.CharField(max_length=32, verbose_name="Тема вопроса")
+    task = models.CharField(max_length=256, verbose_name="Вопрос для пользователя")
+    answer = models.CharField(max_length=256, verbose_name="Ответ на вопрос")
+    reward = models.ForeignKey("Reward", on_delete=models.DO_NOTHING, related_name="RewardOf")
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=32, verbose_name="Имя персонажа")
+    image = models.TextField(verbose_name="Модель персонажа")
+
+
+class User(models.Model):
+    wallet = models.CharField(max_length=42, unique=True, verbose_name="Адрес кошелька пользователя")
+    contract = models.CharField(max_length=42, verbose_name="Контракт пользователя")
+    character = models.ForeignKey("Character", on_delete=models.DO_NOTHING, related_name="CharacterOf")
+    level = models.ForeignKey("Level", on_delete=models.DO_NOTHING, related_name="LevelOf")
